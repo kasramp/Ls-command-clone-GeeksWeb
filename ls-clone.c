512f9fec02592dba3d2a4421db606f8577312431
Kasra Madadipouya <kasra_mp@yahoo.com>
2013-10-09 10:20:10 -0400

    Update ls-clone.c

--- ls-clone.c
+++ ls-clone.c
@@ -1,16 +1,18 @@
 #include <dirent.h>
 #include <stdio.h>
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
     DIR *dir;
     struct dirent *de;
-
-    dir = opendir("/home/kixz"); /*your directory*/
+	if(argc>1)
+	dir=opendir(argv[1]); /*your directory*/
+	else
+    dir = opendir("."); /* Default: current path */
     while(dir)
     {
         de = readdir(dir);
         if (!de) break;
-        printf("%i %s\n", de->d_type, de->d_name);
+        printf("%s\n", de->d_name);
     }
     closedir(dir);
     return 0;
