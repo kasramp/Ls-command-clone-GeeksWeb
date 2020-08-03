dffa0a72ca728fc6d2fe9d0ea39332cd786edc79
Kasra Madadipouya <kasra_mp@yahoo.com>
2020-08-03 21:20:14 +0200

    Minor formatting

--- ls-clone.c
+++ ls-clone.c
@@ -2,18 +2,19 @@
 #include <stdio.h>
 int main(int argc, char *argv[])
 {
-    DIR *dir;
-    struct dirent *de;
-	if(argc>1)
-	dir=opendir(argv[1]); /*your directory*/
-	else
+  DIR *dir;
+  struct dirent *de;
+  if (argc > 1)
+    dir = opendir(argv[1]); /*your directory*/
+  else
     dir = opendir("."); /* Default: current path */
-    while(dir)
-    {
-        de = readdir(dir);
-        if (!de) break;
-        printf("%s\n", de->d_name);
-    }
-    closedir(dir);
-    return 0;
+  while (dir)
+  {
+    de = readdir(dir);
+    if (!de)
+      break;
+    printf("%s\n", de->d_name);
+  }
+  closedir(dir);
+  return 0;
 }
